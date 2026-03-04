5de043c2638c50f4fe5da5e245ab2e34d322d1e9
Lanayyy <yyang26@berklee.edu>
2026-03-03 20:30:24 -0500

    Add files via upload

---
+++ name_length.py
@@ -0,0 +1,8 @@
+# ask for full name
+name = input("What is your full name? ")    
+
+# find hwo many characters it has
+length = len(name)
+
+print("The length is:", length)
+print("The type is:", type(length))
